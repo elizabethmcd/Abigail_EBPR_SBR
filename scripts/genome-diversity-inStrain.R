78edc30e4cb8d27f62669b5b50503e33c7f27f70
Elizabeth McDaniel <emcdaniel@wisc.edu>
2022-06-28 18:35:36 -0400

    label on the figure

--- scripts/genome-diversity-inStrain.R
+++ scripts/genome-diversity-inStrain.R
@@ -38,19 +38,27 @@ names(facet_labels) <- c("Abigail-2021-03-17", "Abigail-2021-04-03")
 
 div_plot <- div_metadata %>% 
   ggplot(aes(x=r2_mean, y=nucl_diversity)) +
-  geom_point(aes(color=group), size=2.5) + 
+  geom_point(aes(color=group), size=3) + 
   scale_color_brewer(palette = "Set3") +
   facet_wrap(~ sample, labeller = labeller(sample = facet_labels)) +
   theme_bw() +
   ylab("Nucleotide Diversity π") +
   xlab(expression(r ^2)) +
-  labs(color=c("Lineage"))
+  labs(color=c("Lineage")) 
+
+label1.1 <- data.frame(r2_mean=0.5, nucl_diversity=.018, lab='Clade IC', sample = factor("Abigail-2021-03-17", levels=c("Abigail-2021-03-17", "Abigail-2021-04-03")))
+
+label1.2 <- data.frame(r2_mean=0.5, nucl_diversity=.0175, lab='Clade IC', sample = factor("Abigail-2021-04-03", levels=c("Abigail-2021-03-17", "Abigail-2021-04-03")))
+
+div_plot_labels <- div_plot + 
+  geom_label(data = label1.1, label="Clade IC") +
+  geom_label(data = label1.2, label="Clade IC")
 
 # Grid of 16S data, relative abundance, and diversity 
-bins_grid <- plot_grid(bar_plot, div_plot, ncol = 2, align="h", axis="b", labels=c("A", "B"))
+bins_grid <- plot_grid(bar_plot, div_plot_labels, ncol = 2, align="h", axis="b", labels=c("A", "B"))
 bins_grid
 
-abigail_bins_grid <- ggarrange(bar_plot, div_plot, labels = c("A", "B"), common.legend=TRUE, legend = "bottom")
+abigail_bins_grid <- ggarrange(bar_plot, div_plot_labels, labels = c("A", "B"), common.legend=TRUE, legend = "bottom")
 abigail_bins_grid
 
 abigail_grid <- plot_grid(amplicon_grid, bins_grid, ncol=1)
