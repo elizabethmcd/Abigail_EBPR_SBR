b80a75ac462d9619d791a577108e6f1244d838e2
Elizabeth McDaniel <emcdaniel@wisc.edu>
2022-07-09 17:16:23 -0700

    fixing covg/qual

--- scripts/genome-diversity-inStrain.R
+++ scripts/genome-diversity-inStrain.R
@@ -27,32 +27,36 @@ div_plot <- left_join(ab_genome_info_filtered, abigail_metadata) %>%
   ggplot(aes(x=r2_mean, y=nucl_diversity)) + geom_point(aes(color=phylum), size=2.5) + facet_wrap(~ sample) + theme_bw() + ylab("Nucleotide Diversity π") + xlab(expression(r ^2)) + labs(color=c("Phylum")) + theme(legend.position = c("bottom"))
 
 
+
 # merge with metadata info for group name 
-colnames(metadata)[1] <- c("genome")
-div_metadata <- left_join(ab_genome_info_filtered, metadata) %>% 
-  select(genome, sample, nucl_diversity, r2_mean, group)
+metadata_info <- read.csv("results/binning/Abigail-bins-info.csv")
+colnames(metadata_info)[1] <- c("genome")
+div_metadata <- left_join(ab_genome_info_filtered, metadata_info) %>% 
+  select(genome, sample, nucl_diversity, r2_mean, group, completeness, contamination)
 
 # plot to match colors of the bar plot of relative abundance 
 facet_labels <- c("Operation Day 43", "Operation Day 60")
 names(facet_labels) <- c("Abigail-2021-03-17", "Abigail-2021-04-03")
 
-div_plot <- div_metadata %>% 
+div_plot <- div_metadata %>%
   ggplot(aes(x=r2_mean, y=nucl_diversity)) +
-  geom_point(aes(color=group), size=3) + 
+  geom_point(aes(color=group), size=4) + 
   scale_color_brewer(palette = "Set3") +
   facet_wrap(~ sample, labeller = labeller(sample = facet_labels)) +
   theme_bw() +
   ylab("Nucleotide Diversity π") +
   xlab(expression(r ^2)) +
   labs(color=c("Lineage")) 
+div_plot
 
-label1.1 <- data.frame(r2_mean=0.5, nucl_diversity=.018, lab='Clade IC', sample = factor("Abigail-2021-03-17", levels=c("Abigail-2021-03-17", "Abigail-2021-04-03")))
+label1.1 <- data.frame(r2_mean=0.5, nucl_diversity=.0182, lab='Clade IC', sample = factor("Abigail-2021-03-17", levels=c("Abigail-2021-03-17", "Abigail-2021-04-03")))
 
-label1.2 <- data.frame(r2_mean=0.5, nucl_diversity=.0175, lab='Clade IC', sample = factor("Abigail-2021-04-03", levels=c("Abigail-2021-03-17", "Abigail-2021-04-03")))
+label1.2 <- data.frame(r2_mean=0.5, nucl_diversity=.0178, lab='Clade IC', sample = factor("Abigail-2021-04-03", levels=c("Abigail-2021-03-17", "Abigail-2021-04-03")))
 
 div_plot_labels <- div_plot + 
-  geom_label(data = label1.1, label="Clade IC") +
-  geom_label(data = label1.2, label="Clade IC")
+  geom_label(data = label1.1, label="Clade IC*") +
+  geom_label(data = label1.2, label="Clade IC*")
+div_plot_labels
 
 # Grid of 16S data, relative abundance, and diversity 
 bins_grid <- plot_grid(bar_plot, div_plot_labels, ncol = 2, align="h", axis="b", labels=c("A", "B"))
